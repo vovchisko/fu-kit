ab2030a33a3e160e7ae834c39a040c750019a213
Vladimir Ishenko <vovchisko@gmail.com>
2021-09-02 02:31:45 +0300

    no tree-shaking :c

---
+++ components.js
@@ -0,0 +1,3 @@
+export { default as FuButton } from './src/components/FuButton.vue'
+export { default as FuSelect } from './src/components/FuSelect.vue'
+export { default as FuText }   from './src/components/FuText.vue'
--- src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as FuButton } from './components/FuButton.vue'
-export { default as FuSelect } from './components/FuSelect.vue'
-export { default as FuText }   from './components/FuText.vue'
