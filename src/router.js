c6e0cec00f8a45e6ee83d3999330e124c2b7eba5
Vladimir Ishenko <vovchisko@gmail.com>
2023-12-06 05:18:31 +0200

    ui-button-route

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import { ROUTE_NAMES } from './route-names.js'
+
 import Home from './pages/Home.vue'
 
 import DocButton         from './pages/DocButton.vue'
@@ -14,25 +16,8 @@ import DocModal          from './pages/DocModal.vue'
 import DocDropdown       from './pages/DocDropdown.vue'
 import DocProgressRadial from './pages/DocProgressRadial.vue'
 import DocIcon           from './pages/DocIcon.vue'
-import DocImg          from './pages/DocImg.vue'
+import DocImg            from './pages/DocImg.vue'
 
-const ROUTE_NAMES = Object.freeze({
-  HOME: 'HOME',
-  DOC_IMG: 'DOC_IMG',
-  DOC_TYPO: 'DOC_TYPO',
-  DOC_BUTTON: 'DOC_BUTTON',
-  DOC_SELECT: 'DOC_SELECT',
-  DOC_DROPDOWN: 'DOC_DROPDOWN',
-  DOC_TEXT: 'DOC_TEXT',
-  DOC_TEXTAREA: 'DOC_TEXTAREA',
-  DOC_CODE_INPUT: 'DOC_CODE_INPUT',
-  DOC_SIDEBAR: 'DOC_SIDEBAR',
-  DOC_CHECK: 'DOC_CHECK',
-  DOC_RADIO: 'DOC_RADIO',
-  DOC_MODAL: 'DOC_MODAL',
-  DOC_PROGRESS_RADIAL: 'DOC_PROGRESS_RADIAL',
-  DOC_ICON: 'DOC_ICON',
-})
 
 const routes = [
   { path: '/', name: ROUTE_NAMES.HOME, component: Home },
