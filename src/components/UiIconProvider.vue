3c12bf71c03bc43755744db4e6a66903db1ea288
Volodymyr Ishchenko <vovchisko@gmail.com>
2024-08-16 21:58:16 +0300

    hotfix icon name

--- src/components/UiIconProvider.vue
+++ src/components/UiIconProvider.vue
@@ -225,13 +225,13 @@
     <symbol id="chevron-up" viewBox="0 0 24 24">
       <path d="M4 16L12 8L20 16" stroke="var(--icon-color, currentColor)" stroke-linecap="round" stroke-linejoin="round" />
     </symbol>
-    <symbol id="chield-check" viewBox="0 0 24 24">
+    <symbol id="shield-check" viewBox="0 0 24 24">
       <path
           d="M3 13V4.22222L4.94372 4.07224C6.6371 3.94158 8.29889 3.54213 9.86666 2.88889L12 2L14.1333 2.88889C15.7011 3.54213 17.3629 3.94158 19.0563 4.07224L21 4.22222V13C21 17.9706 16.9706 22 12 22C7.02944 22 3 17.9706 3 13Z"
           stroke="var(--icon-color, currentColor)" stroke-linecap="round" stroke-linejoin="round" />
       <path d="M8 12L10.5 14.5L16 9" stroke="var(--icon-color, currentColor)" stroke-linecap="round" stroke-linejoin="round" />
     </symbol>
-    <symbol id="chield" viewBox="0 0 24 24">
+    <symbol id="shield" viewBox="0 0 24 24">
       <path
           d="M3 13V4.22222L4.94372 4.07224C6.6371 3.94158 8.29889 3.54213 9.86666 2.88889L12 2L14.1333 2.88889C15.7011 3.54213 17.3629 3.94158 19.0563 4.07224L21 4.22222V13C21 17.9706 16.9706 22 12 22C7.02944 22 3 17.9706 3 13Z"
           stroke="var(--icon-color, currentColor)" stroke-linecap="round" stroke-linejoin="round" />
